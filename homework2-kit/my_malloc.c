d2c76132548658f2cde538486c9d64915cc25624
yang <yg295@duke.edu>
2026-02-12 00:58:04 +0800

    add insert_free_block function

--- homework2-kit/my_malloc.c
+++ homework2-kit/my_malloc.c
@@ -7,3 +7,40 @@ typedef struct block{
 
 #define META_SIZE sizeof(block_t)
 
+static void insert_free_block(block_t **head, block_t * block){
+    //? Why static
+    // block_t** head is the pointer which points to head pointer of the list
+    // block_t * block is the new block, waiting to be inserted
+
+    block_t *curr = *head;
+    block_t *prev = NULL;
+
+    while(curr!= NULL && curr < block){
+        // pointer essentially is the value of an address.
+        // so curr < block means the address of current block is smaller than new block's address
+        prev = curr;
+        curr = curr->next;
+    }
+
+    block->next = curr;
+    if(prev != NULL){
+        prev->next = block;
+    }else{
+        *head = block;
+    }
+
+    //? Why we need to convert to char to compare
+    if(block->next != NULL &&
+    (char *)block + META_SIZE + block->size == (char*)block->next){
+        block->size += META_SIZE  + block->next->size;
+        block->next = block->next->next;
+    }
+
+    if(prev != NULL &&
+    (char*) prev + META_SIZE + prev->size == (char*) block){
+        prev->size += META_SIZE + block->size;
+        prev->next = block->next;
+    }
+
+
+}
